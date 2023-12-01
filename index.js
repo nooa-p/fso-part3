e7c7310bc02c99aaa244bb8b9452d0957d09c3c2
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-01 11:50:44 +0200

    exercise 3.8

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
+morgan.token('body', function getBody (request) {
+    return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
 
 let persons = [
