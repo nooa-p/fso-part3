3f549030b466518a78fea567d7326bf0d7ffff9c
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-05 10:35:41 +0200

    exercise 3.11 build

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', function getBody (request) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 let persons = [
     { 
