8a11ceaa728be3f51c986e01eed1e699be683856
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-07 09:10:14 +0200

    exercise 3.20

--- models/person.js
+++ models/person.js
@@ -22,7 +22,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
       type: String,
-      required: true
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{5,}$/.test(v)
+        }
+      },
+      required: true,
     }
 })
 
